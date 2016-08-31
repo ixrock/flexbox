d863cff8719d8d268293b6cc3a955c5e217f7b65
Roman <ixrock@gmail.com>
2016-08-31 23:49:08 +0300

    splitting code to flexbox-core and flexbox-grid

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,11 @@
 import gulp from 'gulp'
 import autoprefixer from 'gulp-autoprefixer'
 import sass from 'gulp-sass'
-import rename from 'gulp-rename'
 import bs from 'browser-sync'
 
 const srcDir = './src/';
 const docsDir = './docs/';
-const sassFiles = srcDir + '**/*.scss';
+const sassMainFile = srcDir + 'flexbox.scss';
 
 // development watch mode with local server and auto-reloading browsers
 gulp.task('default', () => {
@@ -16,19 +15,17 @@ gulp.task('default', () => {
     startPath: docsDir,
     server: './'
   });
-  gulp.watch(sassFiles, ['sass']);
+  gulp.watch(sassMainFile, ['sass']);
   gulp.watch([
-    docsDir + '**/*.html',
-    docsDir + '**/*.css',
+    docsDir + '**',
     srcDir + '*.css'
   ]).on('change', browserSync.reload);
 });
 
 // compile sass + autoprefixer
 gulp.task('sass', function () {
-  return gulp.src(sassFiles)
+  return gulp.src(sassMainFile)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-    .pipe(rename({extname: '.min.css'}))
     .pipe(gulp.dest(file => file.base));
 });
